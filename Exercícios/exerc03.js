793ad9eca475de92d6c4d9bd50fb08ee6f9db1ed
Dênis <denischarocha@gmail.com>
2024-05-03 13:01:23 -0300

    Resolução Exercício 03

---
+++ Exercícios/exerc03.js
@@ -0,0 +1,14 @@
+prompt = require("prompt-sync")();
+
+function exercicio03(){
+    let distanciaPercorrida = Number(prompt("Digite a distância em KM que você deseja percorrer: "));
+    if(isNaN(distanciaPercorrida)){
+        console.log("Distância Invalida!");
+    }else if(distanciaPercorrida > 200){
+        console.log(`o preço da viagem vai ficar: R$${distanciaPercorrida *0.45}`);
+    }else{
+        console.log(`o preço da viagem vai ficar: R$${distanciaPercorrida *0.5}`);
+    }
+}
+
+exercicio03();
