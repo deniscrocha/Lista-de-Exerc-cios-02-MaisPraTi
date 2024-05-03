0ebc7cf0cd83982f4a3e5727debdad594d9c2e48
Dênis <denischarocha@gmail.com>
2024-05-03 13:15:12 -0300

    Exercício 04 Resolvido

---
+++ Exercícios/exerc04.js
@@ -0,0 +1,21 @@
+prompt = require("prompt-sync")();
+
+
+function exercicio04(){
+    let retas = Array();
+    do{
+        let x = Number(prompt(`Digite a reta número ${retas.length+1}: `));
+        if(!isNaN(x)){
+            retas.push(x);
+        }else{
+            console.log("Número não aceito");
+        }
+    }while(retas.length < 3);
+    if(((retas[0] + retas[1]) > retas[2]) && ((retas[2] + retas[1]) > retas[0]) && ((retas[0] + retas[2]) > retas[1])){
+        console.log("É possível formar um triângulo!");
+    } else{
+        console.log("Não é possível formar um triângulo!");
+    }
+}
+
+exercicio04();
