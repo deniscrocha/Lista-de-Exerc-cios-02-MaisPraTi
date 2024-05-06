db04fc9ec15152de8d33bf671413852db67e80cb
Dênis <denischarocha@gmail.com>
2024-05-06 13:41:59 -0300

    Exercício 13 Solucionado

---
+++ Exercícios/exerc13.js
@@ -0,0 +1,15 @@
+/*
+Crie um programa que preencha automaticamente (usando lógica, não apenas
+atribuindo diretamente) um vetor numérico com 15 posições com os primeiros elementos
+da sequência de Fibonacci.
+*/
+
+function exercicio13(){
+    let fibonacci = Array(0, 1);
+    for(let i = 2; i<15; i++){
+        fibonacci.push(fibonacci[i-1] + fibonacci[i-2]);
+    }
+    console.log(fibonacci);
+}
+
+exercicio13();
