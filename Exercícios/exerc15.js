008db0a80c141a0adb94b84fee516e6a606c6477
Dênis <denischarocha@gmail.com>
2024-05-06 13:54:23 -0300

    Exercício 15 Solucionado

---
+++ Exercícios/exerc15.js
@@ -0,0 +1,21 @@
+/*
+Desenvolva um programa que leia 10 números inteiros e guarde-os em um vetor. No
+final, mostre quais são os números pares que foram digitados e em que posições eles
+estão armazenados.
+*/
+
+const prompt = require("prompt-sync")();
+
+function exercicio15(){
+    let numeros = Array();
+    for(let i = 0; i<10; i++){
+        numeros.push(Number(prompt(`Digite o número posição ${i}: `)));
+    }
+    numeros.forEach((elementos, indice) =>{
+        if((elementos%2) === 0){
+            console.log(`O número ${elementos} é par e foi guardado na posição: ${indice}`);
+        }
+    })
+}
+
+exercicio15();
