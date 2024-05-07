e943edb67ffb2806f1ff007ff03f04128c8858ec
Dênis <denischarocha@gmail.com>
2024-05-07 17:33:22 -0300

    Exercício 25 Solucionado

---
+++ Exercícios/exerc25.js
@@ -0,0 +1,30 @@
+/*
+Faça um algoritmo que leia uma matriz de 15 X 20 de números reais e mostre a soma
+de cada coluna separadamente.
+*/
+
+function exercicio25(){
+    function solucao(matriz){
+        let somasDasColunas = Array();
+        for(let i = 0; i < 15; i++){
+            let soma = matriz[i][0];
+            for(let j = 1; j < 20; j++){
+                soma += matriz[i][j];
+            }
+            somasDasColunas.push(soma);
+        }
+        somasDasColunas.forEach((total, indice)=>{
+            console.log(`A soma total da coluna ${indice+1} foi de : ${total.toFixed(2)}`)
+        })
+    }
+    let matrizTeste = Array();
+    for(let i = 0; i < 15; i++){
+        matrizTeste[i] = Array();
+        for(let j = 0; j < 20; j++){
+            matrizTeste[i].push((Math.random()*1000)-1 - (Math.random()*1000)-1);
+        }
+    }
+    solucao(matrizTeste);
+}
+
+exercicio25();
