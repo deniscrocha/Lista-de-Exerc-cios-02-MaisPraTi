87d73eeadce357b4545c491b2835644c8fddbd2a
Dênis <denischarocha@gmail.com>
2024-05-09 13:31:26 -0300

    Exercício 44 Solucionado

---
+++ Exercícios/exerc44.js
@@ -0,0 +1,20 @@
+/*
+Escreva uma função que conte quantas propriedades do tipo string existem em um
+objeto e retorne esse número.
+*/
+
+function exercicio44(){
+    function solucao(objeto){
+        let camposString = 0;
+        for(campo in objeto){
+            if(typeof objeto[campo] === "string"){
+                camposString++;
+            }
+        }
+        console.log(`Existem no objeto ${camposString} propriedades do tipo`);
+    }
+    let objetoTeste = {nome: "Dênis", sobrenome: "Rocha", idade:23, cidade:"Santa Maria"}
+    solucao(objetoTeste);
+}
+
+exercicio44();
