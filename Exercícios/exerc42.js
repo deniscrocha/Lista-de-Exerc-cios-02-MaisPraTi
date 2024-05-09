4946b2ef1a09497116a8deee6825dc26caccdde0
Dênis <denischarocha@gmail.com>
2024-05-09 13:25:11 -0300

    Exercício 42 Solucionado

---
+++ Exercícios/exerc42.js
@@ -0,0 +1,26 @@
+/*
+Crie um objeto chamado dados que contém várias propriedades, incluindo números,
+strings e arrays. Escreva uma função que retorne um novo objeto apenas com as
+propriedades que são arrays.
+*/
+
+function exercicio42(){
+    function solucao(objeto){
+        let objetoFinal = {}
+        for(campo in objeto){
+            if(Array.isArray(objeto[campo])){
+               objetoFinal[campo] = objeto[campo];
+            }
+        }
+        console.log(objetoFinal);
+    }
+
+    let objetoTeste = {nome:"Joãozinho",
+        idade:32,
+        tecnologias: ['Java', 'JavaScript', 'SQL', 'mongoDB'],
+        telefone: ['Motorola', 'Iphone']
+    }
+    solucao(objetoTeste);
+}
+
+exercicio42();
