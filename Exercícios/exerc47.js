aa43547def5d3c75d05946e897e30c124b235917
Denis <denischarocha@gmail.com>
2024-05-13 15:05:35 -0300

    Exercício 47 Solucionado

---
+++ Exercícios/exerc47.js
@@ -0,0 +1,52 @@
+/*
+Crie uma função que transforme um objeto de entrada aplicando uma função
+fornecida a cada uma das propriedades do objeto, retornando um novo objeto com os
+resultados.
+*/
+
+function exercicio47(){
+    function solucao(entrada, funcaoFornecida){
+        let resposta = 0;
+        switch(funcaoFornecida){
+            case "adicao":
+                for(valor in entrada){
+                    resposta += entrada[valor];
+                }
+                return resposta;
+            case "divisao":
+                for(valor in entrada){
+                    if(resposta === 0){
+                        resposta = entrada[valor];
+                    } else{
+                    resposta /= entrada[valor];
+                }
+                }
+                return resposta;
+            case "multiplicacao":
+                for(valor in entrada){
+                    if(resposta === 0){
+                        resposta = entrada[valor];
+                    } else{
+                        resposta *= entrada[valor];
+                    }
+                }
+                return resposta;
+            case "subtracao":
+                for(valor in entrada){
+                    if(resposta === 0){
+                        resposta = entrada[valor];
+                    }else{
+                    resposta -= entrada[valor];
+                    }
+                }
+                return resposta;
+            default:
+                console.log("Função fornecida não encontrada!")
+        }
+    }
+
+    let objetoDeEntrada = {valorUm: 22, valorDois:3}; 
+    console.log(solucao(objetoDeEntrada, "subtracao"));
+}
+
+exercicio47();
